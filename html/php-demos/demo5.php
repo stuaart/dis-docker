35d8bf471032ee955cf43641f09332a3a3a1c2da
stuaart <stuart.reeves@nottingham.ac.uk>
2024-11-05 22:56:04 +0000

    Added newline.

--- html/php-demos/demo5.php
+++ html/php-demos/demo5.php
@@ -21,7 +21,7 @@
       $result = mysqli_query($conn, $sql);                                       
       if (mysqli_num_rows($result) > 0) {
          while($row = mysqli_fetch_assoc($result)) {
-            echo $row["Name"]."<br/>"; 
+            echo $row["Name"]."<br/>\n"; 
          } 
       }
       else {
